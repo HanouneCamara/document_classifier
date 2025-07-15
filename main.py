9672ff3db878e1f7c9e389c87090ad137bbed0b9
HanouneCamara <Alicmr13@gmail.com>
2025-07-15 12:14:09 +0100

    get-dataloaders

---
+++ main.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+from src.dataset import get_dataloaders
+
+#Charger les DataLoaders
+train_loader, val_loader = get_dataloaders("notebooks/hymenoptera_data", batch_size=4)
+
+#Récupérer un batch
+images, labels = next(iter(train_loader))
+classes = train_loader.dataset.classes
+
+#Afficher le batch
+fig, axs = plt.subplots(1, 4, figsize=(12, 4))
+for i in range(4):
+    img = images[i].permute(1, 2, 0)
+    axs[i].imshow(img)
+    axs[i].set_title(classes[labels[i]])
+    axs[i].axis('off')
+plt.show()
