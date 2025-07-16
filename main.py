65205aa1c5522bd060f2215747c6ce2ef05f6497
HanouneCamara <Alicmr13@gmail.com>
2025-07-16 13:21:43 +0100

    test_build-model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from src.dataset import get_dataloaders
-
+from src.model import build_model
+"""
 #Charger les DataLoaders
 train_loader, val_loader = get_dataloaders("notebooks/hymenoptera_data", batch_size=4)
 
@@ -16,3 +17,14 @@ for i in range(4):
     axs[i].set_title(classes[labels[i]])
     axs[i].axis('off')
 plt.show()
+"""
+
+# Créer le modèle avec 2 classes(ants & bees )
+model = build_model(num_classes=2)
+print(model)
+
+total_params = sum(p.numel() for p in model.parameters())
+trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+print(f"Total parameters: {total_params}")
+print(f"Trainable parameters: {trainable_params}")
