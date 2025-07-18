241cadf07775fcf09aa8abf6489e129b737595ba
HanouneCamara <Alicmr13@gmail.com>
2025-07-18 16:17:26 +0100

    predict-doc

---
+++ src/predict.py
@@ -0,0 +1,53 @@
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+import torch.nn as nn
+import torchvision.models as models
+import argparse
+import os
+
+
+class_names = ['driving_license', 'others', 'social_security']
+# Charge le modèle avec les bons poids
+def load_model(model_path):
+    model = models.resnet18(pretrained=True)
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, len(class_names))
+
+    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+    model.eval()
+    return model
+
+# Fonction de prédiction
+def predict_image(model, image_path):
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], 
+                             [0.229, 0.224, 0.225])
+    ])
+
+    image = Image.open(image_path).convert('RGB')
+    image = transform(image).unsqueeze(0)
+
+    with torch.no_grad():
+        outputs = model(image)
+        _, predicted = torch.max(outputs, 1)
+
+    return class_names[predicted.item()]
+
+# Point d’entrée du script
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Classification de document")
+    parser.add_argument('--image', type=str, required=True, help='Chemin de l’image')
+    parser.add_argument('--model', type=str, default='../models/model.pth', help='Chemin du modèle entraîné')
+    args = parser.parse_args()
+
+    if not os.path.exists(args.image):
+        print("❌ Erreur : image non trouvée :", args.image)
+        exit(1)
+
+    model = load_model(args.model)
+    prediction = predict_image(model, args.image)
+
+    print(f"✅ L’image {os.path.basename(args.image)} est classée comme : {prediction}")
